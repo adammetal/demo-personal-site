d4583fb4be29e7b40dc487b8ea468936f3fb78e3
adammetal <adam.metal@codecool.com>
2024-02-22 15:32:00 +0100

    Updates

--- index.6a31943b.js
+++
@@ -1,4 +0,0 @@
-//window.alert("Hello");
-document.querySelector("#time").innerText += " " + new Date().toLocaleString();
-
-//# sourceMappingURL=index.6a31943b.js.map
--- index.922dce54.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#time").innerText+=" "+new Date().toLocaleString();
-//# sourceMappingURL=index.922dce54.js.map
